7ff6b4c81f5f0b9daf306aec54cc293b861acf30
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-01 10:51:27 +1100

    Display 10 countries untill scolling to bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CountryList from "../components/CountryList/CountryList";
 import Filter from "../components/Filter";
 import LoadingBar from "../components/LoadingBar/LoadingBar";
@@ -8,9 +8,25 @@ import { useCountries } from "../hooks/useCountriesHook";
 import "./Home.css";
 
 const REGION_OPTIONS = ["Africa", "America", "Asia", "Europe", "Oceania"];
-
+const PAGE_LIMIT = 10;
 const Home = () => {
   const { countries, loading, setRegion, setSearchedName } = useCountries();
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const listener = (e: any) => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 100
+      ) {
+        setPage((prev) => prev + 1);
+      }
+    };
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  }, []);
 
   const changeRegionHandler = (event: React.FormEvent<HTMLSelectElement>) => {
     const value = event.currentTarget.value;
@@ -33,7 +49,11 @@ const Home = () => {
         <SearchBar onChange={changeSearchHandler} />
         <Filter options={REGION_OPTIONS} onSelect={changeRegionHandler} />
       </header>
-      {loading ? <LoadingBar /> : <CountryList countries={countries} />}
+      {loading ? (
+        <LoadingBar />
+      ) : (
+        <CountryList countries={countries.slice(0, page * PAGE_LIMIT)} />
+      )}
     </div>
   );
 };
