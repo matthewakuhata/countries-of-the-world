866df07cb614a201c07964202d0a42a106a0dfce
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-30 11:14:23 +1100

    remove app

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import "./App.css";
 const App: React.FC = () => {
   return (
     <div id={"abc"} className="App showme">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 };
