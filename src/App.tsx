7e93ae4b3adf1b13bb6ed371c2fc1a15e7aff6b9
Matthew Akuhata <matthewakuhata@gmail.com>
2022-11-30 11:11:17 +1100

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div id={"abc"} className="App showme">
       <header className="App-header">
